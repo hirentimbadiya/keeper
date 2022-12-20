7c05ce69486c53298e8964780887223121cfdec8
heyhiru <hirentimbadiya74@gmail.com>
2022-12-20 17:10:16 +0530

    Added App component

---
+++ src/components/App.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import CreateArea from "./CreateArea";
+import Footer from "./Footer";
+import Header from "./Header";
+import Note from "./Note";
+
+function App() {
+  const [notes, setNotes] = useState([]);
+
+
+  // function for adding the note to the notes Array 
+  const addNote = (note) => {
+    if (note.title === "") {
+      return;
+    }
+    setNotes((prev) => {
+      return [...prev, note];
+    });
+  };
+
+  // adding note to the notes array when user clicks enter on keyboard
+  const addNoteEnter = (e, note) => {
+    if (e.key === "Enter") {
+      addNote(note);
+    }
+  };
+
+  // removing note from the notes array when user clicks delete button
+  const removeNote = (id) => {
+    setNotes((prev) => {
+      return prev.filter((item, index) => {
+        return index !== id;
+      });
+    });
+  };
+  return (
+    <div>
+      <Header />
+      {/* this contains inputs and add button */}
+      <CreateArea onAddNote={addNote} onAddNoteEnter={addNoteEnter} />
+
+      {/* it displays the content of the Array of object in note */}
+      {notes.map((item, index) => {
+        return (
+          // when user clicks delete button it triggers removeNote function
+          <Note
+            key={index}
+            id={index}
+            title={item.title}
+            content={item.content}
+            onRemoveNote={removeNote}
+          />
+        );
+      })}
+      <Footer />
+    </div>
+  );
+}
+export default App;
