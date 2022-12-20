ea3cbeca3759e7021dd7344222fd62c50f6730d3
heyhiru <hirentimbadiya74@gmail.com>
2022-12-20 17:10:01 +0530

    Added Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <div className="bulbimage">
+        <img src="https://img.icons8.com/dotty/100/null/light-on.png" />{" "}
+      </div>
+      <h1> Keeper 📑 </h1>
+    </header>
+  );
+}
+export default Header;
