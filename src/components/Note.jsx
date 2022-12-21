aa6748a169e717e8c7ee8322f4b61dc2d70f75e7
heyhiru <hirentimbadiya74@gmail.com>
2022-12-21 18:26:16 +0530

    formatted code

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-import DeleteIcon from '@mui/icons-material/Delete';
+import DeleteIcon from "@mui/icons-material/Delete";
 function Note(props) {
   return (
     <div className="note">
       <h1>{props.title}</h1>
       <p>{props.content}</p>
       <button
-      // onclick it triggers the onRemoveNote which is in App.jsx 
-      // which essentially will trigger removeNote function to remove the note
+        // onclick it triggers the onRemoveNote which is in App.jsx
+        // which essentially will trigger removeNote function to remove the note
         onClick={() => {
           props.onRemoveNote(props.id);
         }}
       >
-        <DeleteIcon/>
+        <DeleteIcon />
       </button>
     </div>
   );
